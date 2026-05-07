237a2957d983bcdfdcaa4b66717004b85d51a2db
brynjaloa <brynjaloa@gmail.com>
2026-05-07 11:37:36 +0000

    Add edit and category list paths to URLs

--- artworks/urls.py
+++ artworks/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.artwork_list, name="artwork_list"),
     path("<int:pk>/", views.artwork_detail, name="artwork_detail"),
+    path("<int:pk>/edit/", views.edit_artwork, name="edit_artwork"),
+    path('categories/', views.category_list, name='category_list'),
     path("add/", views.add_artwork, name="add_artwork"),
 ]
